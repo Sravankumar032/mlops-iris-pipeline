35b064dfe2c285e9d3ee7f90a56c06045367a564
Sravankumar032 <sravankumarreddy.gummadikayala@gmail.com>
2025-08-08 18:08:56 +0530

    Completed Part 3: API & Docker Packaging

--- api/app/predict.py
+++ api/app/predict.py
@@ -1,10 +1,19 @@
 import joblib
 import numpy as np
+import pandas as pd
 
 def load_model(model_path: str):
     return joblib.load(model_path)
 
-def make_prediction(model, data: dict):
-    features = np.array([[data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']]])
-    prediction = model.predict(features)[0]
-    return prediction
+LABELS = {0: "setosa", 1: "versicolor", 2: "virginica"}
+
+def make_prediction(model, input_data):
+    df = pd.DataFrame([input_data])
+    df.columns = [
+        "sepal length (cm)",
+        "sepal width (cm)",
+        "petal length (cm)",
+        "petal width (cm)"
+    ]
+    prediction = model.predict(df)
+    return LABELS[int(prediction[0])]  
